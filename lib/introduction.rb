11f2fa581a3c5c2b977c4805df7f6b12ca541c78
Michael Dobak <mdobak44@icloud.com>
2021-02-03 23:42:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,9 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
+<<<<<<< HEAD
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+=======
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+>>>>>>> 1080b6ef2452e20a19b37ba9f375fba3a04a0478
 end
